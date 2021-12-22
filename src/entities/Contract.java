4bd58d4d58c97c2dc06c10dcb850a803ec2a568f
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2021-12-22 17:05:20 -0300

    Installment and Contract

--- src/entities/Contract.java
+++ src/entities/Contract.java
@@ -1,12 +1,16 @@
 package entities;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 public class Contract {
 	
 	private Integer number;
 	private Date date;
 	private Double totalValue;
+	
+	List<Installment> installments = new ArrayList<Installment>();
 	
 	public Contract() {
 		
@@ -42,7 +46,8 @@ public class Contract {
 	public void setTotalValue(Double totalValue) {
 		this.totalValue = totalValue;
 	}
-	
-	
-	
+
+	public List<Installment> getInstallments() {
+		return installments;
+	}
 }
