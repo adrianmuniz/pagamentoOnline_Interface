ad5c20d5b59c2b397f00fe9df5f82d97c8df30f3
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2021-12-26 12:02:56 -0300

    PayPal Service

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,15 @@
 package application;
 
+import services.OnlinePaymentService;
+import services.PayPalService;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		OnlinePaymentService ps = new PayPalService();
+		
+		System.out.println(ps.paymentFee(200.00));
 		
 	}
 
